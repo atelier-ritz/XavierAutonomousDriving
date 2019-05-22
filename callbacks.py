9e4f021cbeef8742579be01e0cb09e93cf36ecb9
atelier-ritz <xutianqish@gmail.com>
2019-05-22 23:29:07 +0000

    Updated dual camera

--- callbacks.py
+++ callbacks.py
@@ -24,7 +24,7 @@ class GUI(QMainWindow,Ui_MainWindow):
 		self.setupTimer()
 		self.setupJoystick()
 		self.setupDac()
-		self.setupCamera()
+		self.setupCameras()
 		self.connectSignals()
 		self.linkWidgets()
 
@@ -33,7 +33,7 @@ class GUI(QMainWindow,Ui_MainWindow):
 		self.quitTimer()
 		self.quitJoystick()
 		self.quitDac()
-		self.quitCamera()
+		self.quitCameras()
 		event.accept()
 
 	def setupTimer(self):
@@ -66,16 +66,21 @@ class GUI(QMainWindow,Ui_MainWindow):
 		self.dac = MCP4728(bus, DEFAULT_ADDRESS)
 			
 	def quitDac(self):
-#		time.sleep(0.1) # need to wait before stop sending signals
 		self.dac.clearAll()
 
-	def setupCamera(self):
+	def setupCameras(self):
 		self.webcam = Webcam(0)
+		self.webcam.streamStarted.connect(self.on_streamStarted)
 		self.webcam.changePixmap.connect(self.on_receive_frame)
 		self.webcam.finished.connect(self.on_webcam_terminated)
+		self.webcam2 = Webcam(1)
+		self.webcam2.streamStarted.connect(self.on_streamStarted2)
+		self.webcam2.changePixmap.connect(self.on_receive_frame2)
+		self.webcam2.finished.connect(self.on_webcam_terminated2)
 	
-	def quitCamera(self):
+	def quitCameras(self):
 		self.webcam.stop()
+		self.webcam2.stop()
 		
 	def update(self):
 		self.updateJoystick()
@@ -88,21 +93,15 @@ class GUI(QMainWindow,Ui_MainWindow):
 				pass
 			else:
 				self.joystick.update()
-				# below is option	
-#				if self.joystick.isPressed('CIRCLE'):		
-#					print '<INFO> Circle is pressed!\r',
-#				if self.joystick.isPressed('SQUARE'):		
-#					print '<INFO> Square is pressed!\r',	
 				self.lbl_joystick_button.setText(self.joystick.getButtonPressed())
 				self.lbl_joystick_lstick.setText('{:2.2f}, {:2.2f}'.format(self.joystick.getAngleLeft(), self.joystick.getTiltLeft()))
 				self.lbl_joystick_rstick.setText('{:2.2f}, {:2.2f}'.format(self.joystick.getAngleRight(), self.joystick.getTiltRight()))
-#				val = int(self.joystick.getMagniudeLeft() * 2890)
-#				self.dac.setVoltage(0, val)
 			
 	def connectSignals(self):
 		''' connect widgets with callback functions '''
 		self.btn_set_A.clicked.connect(self.on_btn_set_A)
 		self.btn_camera.clicked.connect(lambda state: self.on_btn_camera(state))
+		self.btn_camera2.clicked.connect(lambda state: self.on_btn_camera2(state))
 		self.btn_joystick.clicked.connect(lambda state: self.on_btn_joystick(state))
 		
 	def linkWidgets(self):
@@ -113,16 +112,22 @@ class GUI(QMainWindow,Ui_MainWindow):
     #=====================================================  
 	def on_btn_set_A(self):
 		self.dac.setVoltage(0, self.spb_vol_A.value())
-		#mydac.setVoltageSeq()
-	    #print self.spb_vol_A.value()
 	    
 	def on_btn_camera(self, isButtonToggled):
 		if isButtonToggled:
 			print('<INFO> Camera is open.')
-			self.webcam.init()
+			self.btn_camera.setEnabled(False)
 			self.webcam.start()
 		else:
 			self.webcam.stop()
+	
+	def on_btn_camera2(self, isButtonToggled):
+		if isButtonToggled:
+			print('<INFO> Camera2 is open.')
+			self.btn_camera2.setEnabled(False)
+			self.webcam2.start()
+		else:
+			self.webcam2.stop()
 	
 	def on_btn_joystick(self, isButtonToggled):
 		if isButtonToggled:
@@ -131,14 +136,34 @@ class GUI(QMainWindow,Ui_MainWindow):
 		else:
 			print('<INFO> Joystick is disconnected.')
 			self.isJoystickEnabled = False
-	
+			
+	#=====================================================
+    # PyQtslot Callback Functions
+    #===================================================== 
+	@pyqtSlot()
+	def on_streamStarted(self):
+		self.btn_camera.setEnabled(True)
+		
+	@pyqtSlot()
+	def on_streamStarted2(self):
+		self.btn_camera2.setEnabled(True)
+
 	@pyqtSlot(QImage)
 	def on_receive_frame(self, image):
 		self.view_webcam.setPixmap(QPixmap.fromImage(image))
-        
+		
+	@pyqtSlot(QImage)
+	def on_receive_frame2(self, image):
+		self.view_webcam2.setPixmap(QPixmap.fromImage(image))
+
 	@pyqtSlot()
 	def on_webcam_terminated(self):
 		print('<INFO> Webcam is terminated.')
 		self.view_webcam.setText('Camera1')
-		
+
+	@pyqtSlot()
+	def on_webcam_terminated2(self):
+		print('<INFO> Webcam2 is terminated.')
+		self.view_webcam2.setText('Camera2')
+
 		
--- lib/vision.py
+++ lib/vision.py
@@ -2,44 +2,35 @@ import cv2, sys, re, time
 import imgfilter
 from PyQt5.QtCore import pyqtSignal, QMutexLocker, QMutex, QThread, Qt
 from PyQt5.QtGui import QImage
-#=============================================================================================
-# Mouse callback Functions
-#=============================================================================================
-#def showClickedCoordinate(event,x,y,flags,param):
-#    # global mouseX,mouseY
-#    if event == cv2.EVENT_LBUTTONDOWN:
-#        # mouseX,mouseY = x,y
-#        print('Clicked position  x: {} y: {}'.format(x,y))
 
 class Webcam(QThread):
 	changePixmap = pyqtSignal(QImage)
+	streamStarted = pyqtSignal()
 	
 	def __init__(self, index, parent=None,):
 		super(Webcam, self).__init__(parent)
 		self.mutex = QMutex()
 		self.id = index
-
-	def init(self):
 		self.stopped = False
-		self.cap = cv2.VideoCapture(0)
-		self.cap.set(3, 1280)
-		self.cap.set(4, 720)
-		if not self.cap.isOpened():
-			print('Camera is not detected. End program.')
-			self.cap.release()
-			return
-#		cv2.namedWindow(self.windowName(),16) # cv2.GUI_NORMAL = 16
-#		cv2.setMouseCallback(self.windowName(), showClickedCoordinate)
-	
+
 	def stop(self):
 		with QMutexLocker(self.mutex):
 			self.stopped = True
 			
+	def setup(self):
+		self.stopped = False
+		self.cap = cv2.VideoCapture(self.id)
+		self.cap.set(3, 1280)
+		self.cap.set(4, 720)
+		if not self.cap.isOpened():
+			print('Camera{} is not detected. End program.'.format(self.id))
+			self.cap.release()
+			return
+		self.streamStarted.emit()	
+	
 	def run(self):
-#		startTime = time.time()
-#		timestamp = []
+		self.setup()	
 		while True:
-#			timestamp.append(time.time() - startTime)
 			ret, img = self.cap.read()
 			if ret:
 				img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
@@ -49,15 +40,9 @@ class Webcam(QThread):
 				convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
 				p = convertToQtFormat.scaled(640, 360, Qt.KeepAspectRatio)
 				self.changePixmap.emit(p)
-#				cv2.imshow(self.windowName(),img)
 			if self.stopped:
-#				print timestamp
 				self.cap.release()
-#				cv2.destroyWindow(self.windowName())
 				return
-		# Actually, the lines below are not necessary in a while True loop
-#		self.stop()
-#		self.finished.emit()
 
 	def windowName(self):
 		return 'CamID:{} (Click to print coordinate)'.format(self.id)
