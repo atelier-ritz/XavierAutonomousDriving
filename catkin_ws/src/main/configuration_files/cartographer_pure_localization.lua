129e0a6b34472d3047fdd6a5b59e03a1bdf9464c
atelier-ritz <xutianqish@gmail.com>
2019-06-05 19:42:05 -0500

    added online/offline mapping + localization
    use_online_correlative_scan_matching
    pure localization is still not functioning perfectly

--- catkin_ws/src/main/configuration_files/cartographer_pure_localization.lua
+++ catkin_ws/src/main/configuration_files/cartographer_pure_localization.lua
@@ -44,6 +44,9 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
+TRAJECTORY_BUILDER.pure_localization = true
+
+
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
 
 TRAJECTORY_BUILDER_2D.min_range = 0.
@@ -67,4 +70,5 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
+
 return options
